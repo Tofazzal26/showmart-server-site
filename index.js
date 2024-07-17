710f4fab52e6449a2b666b47ca0da0087b774347
Tofazzal26 <afranislamabir6789@gmail.com>
2024-07-17 23:34:43 +0600

    collection oparation finish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 
@@ -17,7 +18,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rgxjhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,11 +29,14 @@ const client = new MongoClient(uri, {
   },
 });
 
+const userRoleCollection = client.db("financity").collection("Users");
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
